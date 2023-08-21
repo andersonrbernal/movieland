c38a2eda9841311b2a5266b880660fa9a8da01a7
Anderson <andersonbernal2017@gmail.com>
2023-08-21 13:26:50 -0400

    chore(i18n): update locales

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,4 +1,5 @@
 export default {
     'metadata.title': 'Movieland',
-    'metadata.description': 'Search for your favorite movies.'
+    'metadata.description': 'Search for your favorite movies.',
+    'home.no-movies': 'No movies to be shown. Try searching for one.'
 } as const;
--- src/locales/pt-br.ts
+++ src/locales/pt-br.ts
@@ -1,4 +1,5 @@
 export default {
     'metadata.title': 'Movieland',
-    'metadata.description': 'Pesquise seus filmes favoritos.'
+    'metadata.description': 'Pesquise seus filmes favoritos.',
+    'home.no-movies': 'Nenhum filme para mostrar. Tente procurar por um.'
 } as const;
